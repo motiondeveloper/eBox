b80165b732521e59b0ca3d9f951f4db37961483e
Tim Haywood <timhay@l-cnu33196hv.ws.sbs.com.au>
2019-05-28 17:06:28 +1000

    feat: return interface object

--- eBox.jsx
+++ eBox.jsx
@@ -1,59 +1,43 @@
 {
-    "Box": function() {
+    "createBox": function(initialPoints) {
 
-        var boxPoints = [[0,0], [0,0], [0,0], [0,0]];
+        let boxPoints = initialPoints;
 
-        this.setSize = function(size) {
-            if (sizeIsValid(size)) {
-                var originalPosition = getPosition(boxPoints);
-                var tempPoints = sizeToPoints(size);
-                boxPoints = movePointsCenter(tempPoints, originalPosition);
-            }
+        setSize = (size) => {
+            const originalPosition = getPosition(boxPoints);
+            const tempPoints = sizeToPoints(size);
+            boxPoints = movePointsCenter(tempPoints, originalPosition);
         }
 
-        this.setPosition = function(position, anchorPoint) {
-            if (positionIsValid(position)) {
-                var tempPoints = copyArray(boxPoints);
-                boxPoints = movePointsCenter(tempPoints, cornerToCenterPosition(position, anchorPoint, tempPoints));
-            }
+        setPosition = (position, anchorPoint) => {
+            const tempPoints = Array.from(boxPoints);
+            boxPoints = movePointsCenter(tempPoints, cornerToCenterPosition(position, anchorPoint, tempPoints));
         }
 
-        this.setScale = function(scale, anchorPoint) {
-            if (scaleIsValid(scale)) {
-                var originalSize = getSize(boxPoints);
-                var originalPosition = getPosition(boxPoints);
-                var scaledSize = [originalSize[0] * (scale[0] / 100), originalSize[1] * (scale[1] / 100)];
-                var scaledPoints = sizeToPoints(scaledSize);
-                boxPoints = movePointsCenter(scaledPoints, originalPosition);
-            }
+        setScale = (scale, anchorPoint) => {
+            const originalSize = getSize(boxPoints);
+            const originalPosition = getPosition(boxPoints);
+            const scaledSize = [originalSize[0] * (scale[0] / 100), originalSize[1] * (scale[1] / 100)];
+            const scaledPoints = sizeToPoints(scaledSize);
+            boxPoints = movePointsCenter(scaledPoints, originalPosition);
         }
 
-        this.show = function() {
-            return pointsToPath(boxPoints);
+        show = () => pointsToPath(boxPoints);
+
+        const getSize = (points) => {
+            return [points[1][0] - points[0][0], points[2][1] - points[1][1]];
         }
 
-        function getSize(points) {
-            var size = [points[1][0] - points[0][0], points[2][1] - points[1][1]];
-            return size;
-        }
-
-        function getPosition(points) {
-            var boxSize = getSize(points)
+        const getPosition = (points) => {
+            const boxSize = getSize(points);
             return points[0] + boxSize/2;
         }
 
-        function movePointsCenter(points, position) {
-            var pointSize = getSize(points);
-            var tempPoints = sizeToPoints(pointSize);
-
-            for (var i=0; i<boxPoints.length; i++) {
-                tempPoints[i] += position;
-            }
-            
-            return tempPoints;
+        const movePointsCenter = (points, position) => {
+            return sizeToPoints(getSize(points)).map(point => point += position);
         }
 
-        function cornerToCenterPosition(position, anchorPoint, points) {
+        const cornerToCenterPosition = (position, anchorPoint, points) => {
 
             var centerPosition = [];
             var boxSize = getSize(points);
@@ -122,32 +106,17 @@
         }
 
         function pointsToPath(points) {
-            var pathPoints = [];
-            for(i=0; i<points.length; i++) {
-                pathPoints.push(fromCompToSurface(points[i]));
-            }
-            
+            const pathPoints = points.map(point => fromCompToSurface(point));
             return createPath(pathPoints, [], [], true);
         }
 
-        function copyArray(originalArray) {
-            var copyArray = [];
-            for (var i = 0; i < originalArray.length; i++) {
-                copyArray[i] = originalArray[i].slice(0);
-            }
-            return copyArray;
+        const interface = {
+            setSize,
+            setPosition,
+            setScale,
+            show,
         }
 
-        function sizeIsValid(size) {
-            return size.length === 2;
-        }
-
-        function positionIsValid(position) {
-            return position.length === 2;
-        }
-
-        function scaleIsValid(scale) {
-            return scale.length === 2;
-        }
+        return interface;
     }
 }
