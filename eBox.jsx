7bffcc7468501cf24dffb357bfe6918aed1891bd
timhaywood <tim@haywood.org>
2019-06-26 16:24:15 +1000

    rewrite: function return based input

--- eBox.jsx
+++ eBox.jsx
@@ -1,122 +1,49 @@
 {
-    "createBox": function(layer, initialPoints = [[0,0], [0,0], [0,0], [0,0]]) {
-
-        let boxPoints = initialPoints;
-
-        const setSize = (size) => {
-            const originalPosition = getPosition(boxPoints);
-            const tempPoints = sizeToPoints(size);
-            boxPoints = movePointsCenter(tempPoints, originalPosition);
-        }
-
-        const setPosition = (position, anchorPoint) => {
-            const tempPoints = [...boxPoints];
-            boxPoints = movePointsCenter(tempPoints, cornerToCenterPosition(position, anchorPoint, tempPoints));
-        }
-
-        const setScale = (scale, anchorPoint) => {
-            const originalSize = getSize(boxPoints);
-            const originalPosition = getPosition(boxPoints);
-            const scaledSize = [originalSize[0] * (scale[0] / 100), originalSize[1] * (scale[1] / 100)];
-            const scaledPoints = sizeToPoints(scaledSize);
-            boxPoints = movePointsCenter(scaledPoints, originalPosition);
-        }
-
-        const getSize = (points) => {
-            return [points[1][0] - points[0][0], points[2][1] - points[1][1]];
-        }
-
-        const getPosition = (points) => {
-            const boxSize = getSize(points);
-            return points[0] + boxSize/2;
-        }
-
-        const movePointsCenter = (points, position) => {
-            return sizeToPoints(getSize(points)).map(point => point += position);
-        }
-
-        const cornerToCenterPosition = (position, anchorPoint, points) => {
-
-            var centerPosition = [];
-            var boxSize = getSize(points);
-
-            switch (anchorPoint) {
-                case 'center':
-                    centerPosition = position;
-                    break;
-                case 'topLeft':
-                    centerPosition = position + [boxSize[0]/2, boxSize[1]/2];
-                    break;
-                case 'topRight':
-                    centerPosition = position + [-boxSize[0]/2, boxSize[1]/2];
-                    break;
-                case 'bottomLeft':
-                    centerPosition = position + [boxSize[0]/2, -boxSize[1]/2];
-                    break;
-                case 'bottomRight':
-                    centerPosition = position + [-boxSize[0]/2, -boxSize[1]/2];
-                    break;
-                default:
-                    break;
-            }
-
-            return centerPosition;
-            
-        }
-
-        function centerToCornerPosition(center, anchorPoint, points) {
-
-            var cornerPosition = [];
-            var boxSize = getSize(points);
-
-            switch (anchorPoint) {
-                case 'center':
-                    cornerPosition = center;
-                    break;
-                case 'topLeft':
-                    cornerPosition = center + [-boxSize[0]/2, -boxSize[1]/2];
-                    break;
-                case 'topRight':
-                    cornerPosition = center + [boxSize[0]/2, -boxSize[1]/2];
-                    break;
-                case 'bottomLeft':
-                    cornerPosition = center + [-boxSize[0]/2, boxSize[1]/2];
-                    break;
-                case 'bottomRight':
-                    cornerPosition = center + [boxSize[0]/2, boxSize[1]/2];
-                    break;
-                default:
-                    break;
-            }
-
-            return cornerPosition;
-            
-        }
-
-        function sizeToPoints(size) {
-            var points = [
-                [-size[0]/2, -size[1]/2],
-                [size[0]/2, -size[1]/2],
-                [size[0]/2, size[1]/2],
-                [-size[0]/2, size[1]/2]
-            ];
-            return points;
-        }
-
-        function pointsToPath(points) {
-            const pathPoints = points.map(point => layer.fromCompToSurface(point));
-            return pathPoints
-;
-        }
-
-        const interface = {
-            setSize,
-            setPosition,
-            setScale,
-            getSize,
-            show: pointsToPath(boxPoints),
-        }
-
-        return interface;
+  "createBox": function(layer, boxProps = {}) {
+    const positionToCenter = (position, size, anchor) => {
+      return {
+        'center': position,
+        'topLeft': position + [size[0]/2, size[1]/2],
+        'topRight': position + [-size[0]/2, size[1]/2],
+        'bottomLeft': position + [size[0]/2, -size[1]/2],
+        'bottomRight': position + [-size[0]/2, -size[1]/2],
+      }[anchor];        
     }
+
+    const sizeToPoints = (size) => {
+      return [
+        [-size[0]/2, -size[1]/2],
+        [size[0]/2, -size[1]/2],
+        [size[0]/2, size[1]/2],
+        [-size[0]/2, size[1]/2]
+      ];
+    }
+    const movePoints = (points, position) => points.map(point => point + position);
+    const pointsToComp = points => points.map(point => layer.toComp(point));
+    const pointsToPath = (points, isClosed) => layer.createPath(points, 0, 0, isClosed);
+
+    function createPathFromBoxProps(boxProps) {
+      const points = sizeToPoints(boxProps.size);
+      const centeredPoints = movePoints(points, boxProps.centerPosition);
+      const compPositionPoints = pointsToComp(centeredPoints);
+      const boxPath = pointsToPath(compPositionPoints, box.isClosed);
+
+      return boxPath;
+    }
+
+    function scaleBox(boxProps, scale = [100, 100]) {
+
+    }
+
+    const box = {
+      size: [500, 500],
+      position: [960, 540],
+      anchor: 'bottomLeft',
+      isClosed: true,
+    }
+
+    box.centerPosition = positionToCenter(box.position, box.size, box.anchor);
+    
+    return createPathFromBoxProps(box);
+  }
 }
