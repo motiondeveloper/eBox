d7d7a77e69e423ed14fa31655bea4df0ddcb30f3
Tim Haywood <48076776+timhaywood@users.noreply.github.com>
2023-04-12 14:02:25 +1000

    convert scaling function to work for more anchors

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,17 @@ const thisLayer = new Layer();
 const thisProperty = new PathProperty([[0, 0]]);
 
 // eBox types
-type Anchor = 'topLeft' | 'topRight' | 'bottomRight' | 'bottomLeft' | 'center';
+type Anchor =
+  | 'topLeft'
+  | 'topCenter'
+  | 'topRight'
+  | 'bottomRight'
+  | 'bottomCenter'
+  | 'bottomLeft'
+  | 'centerLeft'
+  | 'center'
+  | 'centerRight';
+
 type Rounding = [number, number, number, number];
 
 interface BoxProps {
@@ -41,55 +51,116 @@ function createBox({
   function scaleBoxPoints(scale: Vector2D, anchor: Anchor): void {
     // Remap scale to [0..1]
     const normalizedScale: Vector2D = scale.map(
-      scale => scale / 100
+      (scale) => scale / 100
     ) as Vector2D;
 
-    // Get index of anchor point
-    const pointOrder: Anchor[] = [
-      'topLeft',
-      'topRight',
-      'bottomRight',
-      'bottomLeft',
-    ];
-    const anchorPointIndex: number = pointOrder.indexOf(anchor);
-    const anchorPoint: Vector2D = boxPoints[anchorPointIndex];
+    const [pTl, pTr, pBr, pBl] = boxPoints;
+    const boxWidth = pTr[0] - pTl[0];
+    const boxHeight = pBr[1] - pTl[1];
+    const boxCenter = thisLayer.add(pTl, [boxWidth / 2, boxHeight / 2]);
 
-    // Calculate distance from anchor point
-    const pointDeltas: Points = boxPoints.map(point => {
-      return point.map((dimension, dimensionIndex): number => {
-        return dimension - anchorPoint[dimensionIndex];
-      }) as Vector2D;
-    }) as Points;
+    // Get offsets from center for each point
+    const deltasFromCenter = boxPoints.map((point) =>
+      thisLayer.sub(point, boxCenter)
+    );
 
-    // Scale the point deltas according to input scale
-    const scaledPointDeltas: Points = pointDeltas.map(
-      (point): Vector2D => {
-        return point.map((dimension, dimensionIndex): number => {
-          return dimension * normalizedScale[dimensionIndex];
-        }) as Vector2D;
+    boxPoints = getScaledDeltas().map((delta) =>
+      thisLayer.add(boxCenter, delta)
+    );
+
+    function getScaledDeltas() {
+      const [dTl, dTr, dBr, dBl] = deltasFromCenter;
+      const [sx, sy] = normalizedScale;
+
+      /**
+       * Used to multiply a delta so it scales not to zero,
+       * but to -1, for scaling from corner anchors
+       */
+      const doubleMult = (val: number, mul: number) => val * (2 * mul - 1);
+
+      if (anchor === 'topLeft') {
+        const sTl = dTl;
+        const sTr = [doubleMult(dTr[0], sx), dTr[1]];
+        const sBr = [doubleMult(dBr[0], sx), doubleMult(dBr[1], sy)];
+        const sBl = [dBl[0], doubleMult(dBl[1], sy)];
+
+        return [sTl, sTr, sBr, sBl];
       }
-    ) as Points;
 
-    const scaledPoints: Points = boxPoints.map(
-      (point, pointIndex): Vector2D => {
-        if (pointIndex !== anchorPointIndex) {
-          // If not the anchor point
-          // Create the point from the scaledPointDelta
-          return point.map((pointDimension, dimensionIndex): number => {
-            return (
-              anchorPoint[dimensionIndex] +
-              scaledPointDeltas[pointIndex][dimensionIndex]
-            );
-          }) as Vector2D;
-        } else {
-          // If the anchor point
-          // Return as is
-          return point;
-        }
+      if (anchor === 'topRight') {
+        const sTl = [doubleMult(dTl[0], sx), dTl[1]];
+        const sTr = dTr;
+        const sBr = [dBr[0], doubleMult(dBr[1], sy)];
+        const sBl = [doubleMult(dBl[0], sx), doubleMult(dBl[1], sy)];
+
+        return [sTl, sTr, sBr, sBl];
       }
-    ) as Points;
 
-    boxPoints = scaledPoints;
+      if (anchor === 'topCenter') {
+        const sTl = [dTl[0] * sx, dTl[1]];
+        const sTr = [dTr[0] * sx, dTr[1]];
+        const sBr = [dBr[0] * sx, doubleMult(dBr[1], sy)];
+        const sBl = [dBl[0] * sx, doubleMult(dBl[1], sy)];
+
+        return [sTl, sTr, sBr, sBl];
+      }
+
+      if (anchor === 'bottomRight') {
+        const sTl = [doubleMult(dTl[0], sx), doubleMult(dTl[1], sy)];
+        const sTr = [dTr[0], doubleMult(dTr[1], sy)];
+        const sBr = dBr;
+        const sBl = [doubleMult(dBl[0], sx), dBl[1]];
+
+        return [sTl, sTr, sBr, sBl];
+      }
+
+      if (anchor === 'bottomLeft') {
+        const sTl = [dTl[0], doubleMult(dTl[1], sy)];
+        const sTr = [doubleMult(dTr[0], sx), doubleMult(dTr[1], sy)];
+        const sBr = [doubleMult(dBr[0], sx), dBr[1]];
+        const sBl = dBl;
+
+        return [sTl, sTr, sBr, sBl];
+      }
+
+      if (anchor === 'bottomCenter') {
+        const sTl = [dTl[0] * sx, doubleMult(dTl[1], sy)];
+        const sTr = [dTr[0] * sx, doubleMult(dTr[1], sy)];
+        const sBr = [dBr[0] * sx, dBr[1]];
+        const sBl = [dBl[0] * sx, dBl[1]];
+
+        return [sTl, sTr, sBr, sBl];
+      }
+
+      if (anchor === 'centerLeft') {
+        const sTl = [dTl[0], dTl[1] * sy];
+        const sTr = [doubleMult(dTr[0], sx), dTr[1] * sy];
+        const sBr = [doubleMult(dBr[0], sx), dBr[1] * sy];
+        const sBl = [dBl[0], dBl[1] * sy];
+
+        return [sTl, sTr, sBr, sBl];
+      }
+
+      if (anchor === 'centerRight') {
+        const sTl = [doubleMult(dTl[0], sx), dTl[1] * sy];
+        const sTr = [dTr[0], dTr[1] * sy];
+        const sBr = [dBr[0], dBr[1] * sy];
+        const sBl = [doubleMult(dBl[0], sx), dBl[1] * sy];
+
+        return [sTl, sTr, sBr, sBl];
+      }
+
+      /**
+       * anchor === 'center'
+       * no doubling needed, just scale
+       * each delta
+       */
+      return deltasFromCenter.map((delta) => {
+        return delta.map((dimension, index) => {
+          return dimension * normalizedScale[index];
+        });
+      });
+    }
   }
 
   function getRoundedPathForPoints(points: Points) {
@@ -98,7 +169,7 @@ function createBox({
     const width = pTr[0] - pTl[0];
     const height = pBl[1] - pTl[1];
 
-    const [rTl, rTr, rBr, rBl] = rounding.map(radius => {
+    const [rTl, rTr, rBr, rBl] = rounding.map((radius) => {
       const minDimension = Math.min(width, height);
       return Math.max(Math.min(minDimension / 2, radius), 0);
     });
@@ -118,35 +189,39 @@ function createBox({
       thisLayer.add(pBl, [0, -rBl]),
     ];
 
-    const inTangents = ([
-      // Top left
-      [0, 0],
-      [-rTl, 0],
-      // Top right
-      [0, 0],
-      [0, -rTr],
-      // Bottom right
-      [0, 0],
-      [rBr, 0],
-      // Bottom left
-      [0, 0],
-      [0, rBl],
-    ] as Vector2D[]).map(p => thisLayer.mul(p, tangentMult));
+    const inTangents = (
+      [
+        // Top left
+        [0, 0],
+        [-rTl, 0],
+        // Top right
+        [0, 0],
+        [0, -rTr],
+        // Bottom right
+        [0, 0],
+        [rBr, 0],
+        // Bottom left
+        [0, 0],
+        [0, rBl],
+      ] as Vector2D[]
+    ).map((p) => thisLayer.mul(p, tangentMult));
 
-    const outTangents = ([
-      // Top left
-      [0, -rTl],
-      [0, 0],
-      // Top right
-      [rTr, 0],
-      [0, 0],
-      // Bottom right
-      [0, rBr],
-      [0, 0],
-      // Bottom left
-      [-rBl, 0],
-      [0, 0],
-    ] as Vector2D[]).map(p => thisLayer.mul(p, tangentMult));
+    const outTangents = (
+      [
+        // Top left
+        [0, -rTl],
+        [0, 0],
+        // Top right
+        [rTr, 0],
+        [0, 0],
+        // Bottom right
+        [0, rBr],
+        [0, 0],
+        // Bottom left
+        [-rBl, 0],
+        [0, 0],
+      ] as Vector2D[]
+    ).map((p) => thisLayer.mul(p, tangentMult));
 
     return thisProperty.createPath(
       cornerPoints,
@@ -167,19 +242,23 @@ function createBox({
     anchor: Anchor
   ): Vector2D {
     const positionCalculations = {
-      center: (): Vector2D => position,
       topLeft: (): Vector2D => [
         position[0] + size[0] / 2,
         position[1] + size[1] / 2,
       ],
+      topCenter: (): Vector2D => [position[0], position[1] + size[1] / 2],
       topRight: (): Vector2D => [
         position[0] - size[0] / 2,
         position[1] + size[1] / 2,
       ],
+      centerLeft: (): Vector2D => [position[0] + size[1] / 2, position[1]],
+      center: (): Vector2D => position,
+      centerRight: (): Vector2D => [position[0] - size[1] / 2, position[1]],
       bottomLeft: (): Vector2D => [
         position[0] + size[0] / 2,
         position[1] - size[1] / 2,
       ],
+      bottomCenter: (): Vector2D => [position[0], position[1] - size[1] / 2],
       bottomRight: (): Vector2D => [
         position[0] - size[0] / 2,
         position[1] - size[1] / 2,
@@ -209,13 +288,11 @@ function createBox({
       }
     ) as Vector2D;
 
-    return points.map(
-      (point: Vector2D): Vector2D => {
-        return point.map((dimension, dimensionIndex) => {
-          return dimension + positionDelta[dimensionIndex];
-        }) as Vector2D;
-      }
-    ) as Points;
+    return points.map((point: Vector2D): Vector2D => {
+      return point.map((dimension, dimensionIndex) => {
+        return dimension + positionDelta[dimensionIndex];
+      }) as Vector2D;
+    }) as Points;
   }
 
   function pointsToComp(points: Points): Points {
